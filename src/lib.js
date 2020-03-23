1eeef2edb30efe48d89938ea87ac6d5f7d504ed3
ncohenpreempt <34206488+ncohenpreempt@users.noreply.github.com>
2020-03-23 16:19:09 +0200

    Update lib.js
    remove enum

--- src/lib.js
+++ src/lib.js
@@ -1,17 +1,9 @@
-export const WEATHER_STATES = {
-    CLEAR: "c",
-    LIGHT_CLOUD: "lc",
-    HEAVY_CLOUD: "hc",
-    LIGHT_RAIN: "lr",
-    HEAVY_RAIN: "hr"
-};
-
 export const WEATHER_STATES_TO_ICON = {
-    [WEATHER_STATES.CLEAR]: "fas fa-sun",
-    [WEATHER_STATES.LIGHT_CLOUD]: "fas fa-cloud-sun",
-    [WEATHER_STATES.HEAVY_CLOUD]: "fas fa-cloud",
-    [WEATHER_STATES.LIGHT_RAIN]: "fas fa-cloud-sun-rain",
-    [WEATHER_STATES.HEAVY_RAIN]: "fas fa-cloud-showers-heavy"
+    c: "fas fa-sun",
+    lc: "fas fa-cloud-sun",
+    hc: "fas fa-cloud",
+    lr: "fas fa-cloud-sun-rain",
+    hr: "fas fa-cloud-showers-heavy"
 };
 
 export const weatherProps = [
